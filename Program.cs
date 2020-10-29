967b9d8174c98873a0352f24b70399b6db74435a
Дмитрий Маркин <dima-markin-2000@yandex.ru>
2020-10-29 18:54:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+
+namespace СocktailParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments(@"--user-data-dir=C:\Users\Maximus\AppData\Local\Google\Chrome\User Data");
+            ChromeDriver driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://ru.inshaker.com/cocktails");
+        }
+    }
+}
